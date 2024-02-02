afcf096ddafa33acb1595640b11505e5eb3cd35a
Monirul Islam <monirul.mahmud@northsouth.edu>
2024-02-02 21:51:48 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from util import predictor
 import smtplib
 
 
+
 # Function to run the image classifier
 def run_image_classifier():
-    st.title("Predict Foot Ulcers")
+    st.markdown('<div style="text-align: center;"><h1 style="color: #F2F7A1;">Predict Foot Ulcer</h1></div>', unsafe_allow_html=True)
+
 
     # Sidebar for user input
     crop_image = st.sidebar.checkbox("Crop Image", value=False)
@@ -41,7 +43,8 @@ def run_image_classifier():
 
 # Function for the contact page
 def contact_page():
-    st.title("Contact Page")
+    st.markdown('<div style="text-align: center;"><h1 style="color: #F2F7A1;">Contact us</h1></div>', unsafe_allow_html=True)
+
 
     # Form for user input
     with st.form("contact_form"):
@@ -93,8 +96,10 @@ def send_email(email, message):
 # Function to create a two-column layout with an image and text
         
 def homepage():
-    st.title("Diabetic Foot Ulcer Detection")
-    st.markdown("Developed and maintain by Monirul, Hafeza and Olid")
+    st.markdown('<div style="text-align: center;"><h1 style="color: #F2F7A1;">Diabetic Foot Ulcer Detection</h1></div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: center;"><h5 style="color: #A5D7E8;">Developed and maintained by Monirul, Hafeza and Olid</h5></div>', unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+
     # Create a two-column layout
     col1, col2 = st.columns(2)
 
@@ -105,19 +110,29 @@ def homepage():
 
     # Column 2: Display text
     with col2:
-        st.write(
+        st.markdown(
             """
+            <div style="text-align: justify;">
             Diabetic Foot Ulcers (DFUs) are a common complication of diabetes and can lead to serious consequences 
-            if not detected and treated early. Our web app leverages machine learning to assist in the early detection 
+            if not detected and treated early. In recent years, the intersection of healthcare and cutting-edge technology 
+            has paved the way for transformative advancements, offering innovative solutions to longstanding challenges. 
+            One such critical issue is the early detection and management of diabetic foot ulcers, a complication that significantly 
+            impacts the quality of life for individuals living with diabetes. In response to this pressing healthcare concern, the 
+            present project, titled "Diabetic Foot Ulcer Detection using Machine Learning," embarks on a journey to leverage the 
+            prowess of state-of-the-art technologies to revolutionize the diagnostic landscape. Our web app leverages machine learning 
+            to assist in the early detection 
             of diabetic foot ulcers through the analysis of uploaded images. By using advanced image classification 
             techniques, our app aims to provide a quick and accurate assessment, helping individuals and healthcare 
             professionals in making informed decisions about preventive measures and treatments.
 
-            Features:
-            - Upload an image to check for the presence of foot ulcers.
-            - Optional cropping of the uploaded image for better analysis.
-            - Receive real-time predictions with probabilities.
-            """
+        At its core, this project employs a sophisticated ensemble of Deep Learning models, namely EfficientNet, DenseNet, and MobileNet, to address the complexities inherent in diabetic foot ulcer detection. The utilization of these models underscores a commitment to harnessing the power of neural networks, known for their capability to discern intricate patterns and features within medical images, thereby facilitating precise and early identification of diabetic foot ulcers. A pivotal aspect of the project lies in its emphasis on interpretability and transparency. To demystify the decision-making processes of the deep learning models employed, the project incorporates Explainable AI, specifically Local Interpretable Model-agnostic Explanations (LIME). By providing insights into the rationale behind each prediction, LIME not only enhances the trustworthiness of the model but also aids healthcare practitioners in understanding and validating the diagnostic outcomes.
+
+            
+        <br>Features:
+        - Upload an image to check for the presence of foot ulcers.
+        - Optional cropping of the uploaded image for better analysis.
+        - Receive real-time predictions with probabilities.            </div>
+            """, unsafe_allow_html=True
         )
 
 
@@ -126,8 +141,10 @@ def homepage():
 # Main function to control the app
 def main():
 
+    st.set_page_config(page_title='CSE 498R - DFU',  layout = 'wide', initial_sidebar_state = 'auto')
+    # favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) or a string (url or local file path)
     # Create radio buttons in the sidebar for navigation
-    selected_page = st.sidebar.radio("Navigation", ["Home", "Predict", "Contact"])
+    selected_page = st.sidebar.radio("Menu", ["Home", "Predict", "Contact"])
 
     # Show content based on the selected radio button
     if selected_page == "Home":
